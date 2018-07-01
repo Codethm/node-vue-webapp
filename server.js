743cb570a25ad32eafe514e9756d5477a16fde83
Codethm <dong55.102375@gmail.com>
2018-07-01 14:53:09 +0700

    https successful

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
-var express=require('express');
-var app=express();
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
 
+var app = express();
 app.use(express.static(__dirname));
 
-app.post('/',function(req,res){
+
+const httpsOptions = {
+    ca: fs.readFileSync(__dirname + '/ssl/codethm_me.ca-bundle'),
+    key: fs.readFileSync(__dirname + '/ssl/codethm.me.key'),
+    cert: fs.readFileSync(__dirname + '/ssl/codethm_me.crt')
+}
+
+app.get('/',function(req,res){
     res.sendfile(__dirname + '/index.html');
 })
 
-app.listen(80)
-console.log("My Service is listening to port 80.");
+app.get('/.well-known/pki-validation/146B5DEB5A1B5AA2E633DF1A17F6D4F2.txt',function(req,res){
+    res.sendfile(__dirname + '/ssl/146B5DEB5A1B5AA2E633DF1A17F6D4F2.txt');
+})
 
+app.listen(80, function(){
+    console.log("HTTP is listening to port 80.");
+});
+
+https.createServer(httpsOptions, app).listen(443,function(){
+    console.log("HTTPS is listening to port 443.");
+});
